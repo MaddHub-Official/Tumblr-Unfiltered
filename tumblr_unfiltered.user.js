f586bb8a6d98dcdee43116523a157ede7acdba74
maddjester <65257955+maddjester@users.noreply.github.com>
2024-02-17 02:56:24 -0500

    Changes to be committed: modified:   LICENSE modified:   tumblr_unfiltered.user.js

--- tumblr_unfiltered.user.js
+++ tumblr_unfiltered.user.js
@@ -1,23 +1,19 @@
 // ==UserScript==
 // @name        Tumblr: Unfiltered
-// @icon        https://cdn.discordapp.com/attachments/1158074207758995467/1158075364623523880/logo.png?ex=65db683a&is=65c8f33a&hm=96709f58bdafcfd8e8987fadb3ac688239f85cbd1a8e369b09db63083baa796e&
+// @icon        logo
 // @namespace   MaddHub
 // @match       https://*.tumblr.com/*
 // @grant       GM_addStyle
+// @resource logo https://cdn.discordapp.com/attachments/1158074207758995467/1158075364623523880/logo.png?ex=65db683a&is=65c8f33a&hm=96709f58bdafcfd8e8987fadb3ac688239f85cbd1a8e369b09db63083baa796e&
 // @license     MIT
-// @version     1.1.0
+// @version     1.2.0
 // @author      maddjester
-// @description 2/16/2024, 5:25:08 PM - A simple CSS tweak to remove the mature warning filter overlays on flagged tumbler images and videos.
+// @description A simple CSS tweak to remove the mature warning filter overlays on flagged tumbler images and videos.
 // @downloadURL https://update.greasyfork.org/scripts/487478/Tumblr%20Unfiltered.user.js
 // @updateURL   https://update.greasyfork.org/scripts/487478/Tumblr%20Unfiltered.user.js
 // @supportURL  https://discord.com/servers/maddhub-official-906957649956991036
 // ==/UserScript==
-
-
 (function() {
     'use strict';
     GM_addStyle('div[data-testid="community-label-cover"] { display: none !important; }');
 })();
-
-
-
