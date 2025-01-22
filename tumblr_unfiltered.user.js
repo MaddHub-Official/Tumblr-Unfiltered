f87a256f93146b0cbc55484aebcc69f0cfa41115
maddjester <65257955+maddjester@users.noreply.github.com>
2025-01-21 22:19:59 -0500

    update tumblr_unfiltered.user.js

--- tumblr_unfiltered.user.js
+++ tumblr_unfiltered.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        Tumblr: Unfiltered
-// @icon        https://primary.jwwb.nl/public/g/i/o/temp-ckosctcwkptnpwfunkxo/l3y0wp/image-17.png?enable-io=true&enable=downscale&width=16
+// @icon        https://primary.jwwb.nl/public/g/i/o/temp-ckosctcwkptnpwfunkxo/l3y0wp/image-17.png?enable-io=true&enable=upscale&width=64
 // @namespace   MaddHub
 // @match       https://*.tumblr.com/*
 // @grant       GM_addStyle
 // @license     MIT
-// @version     1.4.4
+// @version     1.4.5
 // @author      maddjester
 // @description A simple CSS tweak to remove the mature warning filter overlays on flagged tumbler images and videos.
 // @updateURL   https://github.com/MaddHub-Official/Tumblr-Unfiltered/blob/main/tumblr_unfiltered.user.js
